abee558ec124d9a7c4df4c308c935a528a5a3105
Shiv yadav <yshiv4074@gmail.com>
2024-03-22 15:11:11 +0530

    add sumofTwo Fn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ console.log("hello world !");
 function addNewFn(){
     console.log("welcom to my home !");
 }
+
+function sumTwoNumber(a,b){
+    const sum = a + b;
+    console.log("Sum of two number is : ",sum);
+}
